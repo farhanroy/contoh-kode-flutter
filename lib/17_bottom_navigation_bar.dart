f4fb880a53b4e84c9a5bf0830d77bd2a1f004f94
Farhan Roy <farhanroy120@gmail.com>
2021-12-13 15:46:17 +0700

    belajar membuat bottom navigation bar

---
+++ lib/17_bottom_navigation_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class BelajarBottomNavBar extends StatefulWidget {
+  const BelajarBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BelajarBottomNavBar> createState() => _BelajarBottomNavBarState();
+}
+
+class _BelajarBottomNavBarState extends State<BelajarBottomNavBar> {
+  int _selectedNavbar = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bottom Nav Bar"),
+      ),
+      body: IndexedStack(
+          index: _selectedNavbar,
+          children: <Widget>[
+            Text("Halaman Beranda"),
+            Text("Halaman Pesanan"),
+            Text("Halaman Inbox"),
+            Text("Halaman Pengaturan"),
+          ]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Pesanan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Inbox',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Pengaturan',
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
+      ),
+    );
+  }
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+}
